e93efd048b6326b546968845dd74dabf332b6904
maxhaesslein <git@maxhaesslein.de>
2023-08-27 15:32:51 +0200

    fix for overview route

--- system/site/templates/overview.php
+++ system/site/templates/overview.php
@@ -7,7 +7,7 @@ snippet( 'header' );
 echo '<h1>Overview</h1>';
 
 // TODO
-$gallery_slug = $core->route->get_route('request')[0];
+$gallery_slug = $core->route->get('request')[0];
 $gallery = $core->galleries->get_gallery($gallery_slug);
 $images = $gallery->get_images();
 
