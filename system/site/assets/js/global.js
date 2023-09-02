780e08f882929611631bacb38b88a84ced8d3d9e
maxhaesslein <git@maxhaesslein.de>
2023-09-02 12:33:30 +0200

    on single image view, hide cursor & meta menu after a short delay

--- system/site/assets/js/global.js
+++ system/site/assets/js/global.js
@@ -1,5 +1,36 @@
 (function(){
 	
-	console.log('hello world!');
+var hideCursor = {
+
+	delay: 2000,
+	timer: false,
+
+	init: function(){
+
+		hideCursor.startTimeout();
+
+		document.addEventListener( 'mousemove', hideCursor.showCursor );
+	},
+
+	startTimeout: function(){
+		hideCursor.timer = setTimeout(function(){
+			document.body.classList.add('cursor-hidden');
+		}, hideCursor.delay);
+	},
+
+	showCursor: function(){
+		clearTimeout(hideCursor.timer);
+		document.body.classList.remove('cursor-hidden');
+		hideCursor.startTimeout();
+	}
+
+};
+
+function init() {
+	hideCursor.init();
+};
+
+
+window.addEventListener( 'load', init );
 
 })();
