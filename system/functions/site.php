069e8f833b053567bb4804404817c08d310ae088
maxhaesslein <git@maxhaesslein.de>
2023-09-24 12:49:36 +0200

    preload loading animation

--- system/functions/site.php
+++ system/functions/site.php
@@ -113,6 +113,10 @@ function head() {
 	<title><?= get_site_title() ?></title>
 	<?= get_site_sharing_tags() ?>
 <?php
+	// preload loading animation:
+	?>
+	<link rel="preload" href="<?= url('system/site/assets/img/loading_white.svg', false) ?>" as="image" type="image/svg" crossorigin>
+	<?php
 
 	// CSS
 	$css_filter = 'extension=css';
