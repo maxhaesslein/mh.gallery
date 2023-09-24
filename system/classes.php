4e8d34111d2e1a75fa6876ca77baad243fcb2dd4
maxhaesslein <git@maxhaesslein.de>
2023-09-24 12:44:09 +0200

    use PHPs build-in autoloader to load class files
    thanks to @pwaldhauer for pointing me in that direction

--- system/classes.php
+++ system/classes.php
@@ -1,16 +1,6 @@
 <?php
 
-$dir = $abspath.'system/classes/';
-// CLEANUP: include all the relevant files by name instead of including all files in the $dir to make the code a bit safer
-if( $handle = opendir($dir) ){
-	while( false !== ($file = readdir($handle)) ){
-		if( '.' === $file ) continue;
-		if( '..' === $file ) continue;
-
-		$file_extension = pathinfo( $dir.$file, PATHINFO_EXTENSION );
-		if( strtolower($file_extension) != 'php' ) continue;
-
-		include_once( $dir.$file );
-	}
-	closedir($handle);
-}
+spl_autoload_register( function($class_name) {
+	global $abspath;
+	include $abspath.'system/classes/'.strtolower($class_name).'.php';
+} );
